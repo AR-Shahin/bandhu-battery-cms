1cad7b15f8e4cff9108c2ce07d1f78f360320d28
AR-Shahin <mdshahinmije96@gmail.com>
2025-10-03 19:36:35 +0600

    Quick issue fix

--- resources/views/layouts/front_app.blade.php
+++ resources/views/layouts/front_app.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>@yield("title", "Shop") | Shop</title>
+    <title>@yield("title", "Bandhu Battery") | Bandhu Battery</title>
 
     <meta name="description" content="{{ $website->meta }}">
     <meta name="keywords" content="{{ $website->tags }}">
